50ca214eaf2c995f469ad23af030082e2227ad08
Douglas <daglach7@gmail.com>
2018-09-01 12:42:07 -0700

    Add the folder reception [#160199564]

--- registration_checker.py
+++ registration_checker.py
@@ -1,15 +1,32 @@
-f = open("VIP.txt", "r")
-f = open("ORD.txt", "r")
+vip = []
+ordinary = []
 
-def Checker():
+print('--------------------------------')
+
+with open('vip.txt', 'r') as f:
+    data = f.readlines()
+    for line in data:
+        names = line.strip()
+        vip.append(names)
+
+with open('ordinary.txt', 'r') as f:
+    data = f.readlines()
+    for line in data:
+        names = line.strip()
+        ordinary.append(names)
+
+def Checker(name):
     print("Enter your username: ")
     username = input()
-    if f.mode == 'r':
-        names = f.read()
-        if username in names:
-            print(username)
-        else:
-            print("User not in list")
+
+    print('--------------------------------')
+    
+    if username in vip:
+        print('User is registered')
+    else:
+        print('User not registered')
+
+    print('--------------------------------')
 
 if __name__ == "__main__":
-    Checker()
+    Checker(name)
