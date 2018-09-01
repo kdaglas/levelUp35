c3d785f2fc02c223f6ff88368716b19d91e7bcc3
Douglas <daglach7@gmail.com>
2018-08-31 17:46:45 -0700

    Add the registration_checker file

--- registration_checker.py
+++ registration_checker.py
@@ -1,2 +1,15 @@
-print("Enter your username:\n")
-username = input()
+f = open("VIP.txt", "r")
+f = open("ORD.txt", "r")
+
+def Checker():
+    print("Enter your username: ")
+    username = input()
+    if f.mode == 'r':
+        names = f.read()
+        if username in names:
+            print(username)
+        else:
+            print("User not in list")
+
+if __name__ == "__main__":
+    Checker()
